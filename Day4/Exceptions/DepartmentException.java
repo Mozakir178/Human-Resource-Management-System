802a941a0199735e6b0223c37b7fd75c9a97ea4b
Mo Zakir <mozakir178@gmail.com>
2022-09-30 14:04:07 +0530

    Exception handaling

---
+++ Day4/Exceptions/DepartmentException.java
@@ -0,0 +1,18 @@
+package com.hrms.Exceptions;
+
+public class DepartmentException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public DepartmentException() {
+		
+	}
+	
+	public DepartmentException(String msg) {
+			super(msg) ;
+		}
+
+}
